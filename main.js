8f262d4ef5b5bedb91ebad4fd06fa865cf53e664
unknown <iankovychbogdan@gmail.com>
2025-08-11 14:34:07 +0100

    Adding things and re-modeling, Added: new 3 search engines, anti-tracking (still under development) and dark mode (under dev too)

--- main.js
+++ main.js
@@ -2,25 +2,27 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 function createWindow() {
-    const mainWindow = new BrowserWindow({
-        width: 1200,
-        height: 800,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            webviewTag: true,  // <-- enable webview
-        }
-    });
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webviewTag: true,
+      // Note: JS enabled by default, disabling requires window recreate
+      javascript: true,
+    }
+  });
 
-    mainWindow.loadFile(path.join(__dirname, 'homepage.html'));
+  mainWindow.loadFile(path.join(__dirname, 'homepage.html'));
 }
 
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
