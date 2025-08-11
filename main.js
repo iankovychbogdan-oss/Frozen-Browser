1fe8c5d03c18be33e17fbf184602a564fc65bbca
unknown <iankovychbogdan@gmail.com>
2025-08-11 16:20:26 +0100

    New Clean Project

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,       // enable Node.js in webview
+      contextIsolation: false,     // disable context isolation
+      webviewTag: true,
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'homepage.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
